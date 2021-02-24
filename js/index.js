fa3615d1992ede9a012efcd61cd0c64436fae178
Riya Gupta git config --global user.email riyag0841@gmail.com <riyaaaaaaaa8@gmail.com>
2021-02-24 10:03:30 +0530

    added title

--- js/index.js
+++ js/index.js
@@ -1,53 +1,59 @@
 showNotes();
+const title = document.querySelector("#title");
 const textArea = document.querySelector("#note-text");
 const button = document.querySelector(".note-btn");
-button.addEventListener("click", function(e) {
+button.addEventListener("click", function (e) {
     let notes = localStorage.getItem("notes");
-    if(notes == null) {
+    if (notes == null) {
         notesObj = [];
     } else {
         notesObj = JSON.parse(notes);
     }
-    notesObj.push(textArea.value);
+
+    let myObj = {
+        title: title.value,
+        textArea: textArea.value
+    }
+
+    notesObj.push(myObj);
     localStorage.setItem("notes", JSON.stringify(notesObj));
-    textArea.value='';
-    
+    textArea.value = '';
     showNotes();
 });
 
 function showNotes() {
 
     let notes = localStorage.getItem("notes");
-    if(notes == null) {
+    if (notes == null) {
         notesObj = [];
     } else {
         notesObj = JSON.parse(notes);
     }
 
-    let html='';
-    notesObj.forEach(function(element, index) {
-        html += `
+    let html = '';
+    notesObj.forEach(function (element, index) {
+            html += `
         <div>
         <div class="card">
-            <h2>Note ${index + 1} </h2>
-            <p>${element}</p>
+            <h2>${element.title} </h2>
+            <p>${element.textArea}</p>
             <button onClick="deleteNote(this.id)" id="${index}">Delete Note</button>
         </div>
         </div>`;
     });
 
     const insertNotes = document.getElementById("notes");
-    if(notesObj.length !== null) {
-        insertNotes.innerHTML = html;
-    } else {
+    if (notesObj.length == 0) {
         insertNotes.innerHTML = `Nothing to show! Please click on "Add a Note" button to add a new note.`
-    } 
+    } else {
+        insertNotes.innerHTML = html;
+    }
 }
 
 
 function deleteNote(index) {
     let notes = localStorage.getItem("notes");
-    if(notes == null) {
+    if (notes == null) {
         notesObj = [];
     } else {
         notesObj = JSON.parse(notes);
@@ -59,17 +65,17 @@ function deleteNote(index) {
 
 
 let searchTxt = document.getElementById("searchTxt");
-searchTxt.addEventListener("input", function() {
+searchTxt.addEventListener("input", function () {
     let inputVal = searchTxt.value;
     console.log("input event fired", inputVal);
     let cards = document.getElementsByClassName("card");
-    Array.from(cards).forEach(function(element) {
+    Array.from(cards).forEach(function (element) {
         let cardTxt = element.getElementsByTagName('p')[0].innerHTML;
-        if(cardTxt.includes(inputVal)) {
+        if (cardTxt.includes(inputVal)) {
             element.style.display = "block";
         } else {
             element.style.display = "none";
         }
-        })
     })
+})
 
